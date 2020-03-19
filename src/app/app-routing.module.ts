382191586904405f1fcb8ade4a86d14df4b5563d
QINYA <QYZ@QINYAs-MacBook-Air.local>
2020-03-19 11:08:32 -0400

    add token generation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { TokenLoginComponent } from './token-login/token-login.component';
 import { SetupComponent } from './setup/setup.component';
 import { HomeComponent } from './shared/component/home/home.component';
+import {TokenComponent} from './generate-token/token.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'token', component: TokenLoginComponent},
   { path: 'setup', component: SetupComponent},
   { path: 'home', component: HomeComponent},
+  { path: 'tokens', component: TokenComponent },
   { path: '**', redirectTo: '/login'}
 ];
 
